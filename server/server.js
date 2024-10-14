5e52cef931fad5cc5567aec04312ef925b8d8e63
Rahman1014 <Rahman1014@outlook.com>
2024-10-14 17:42:33 +0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -23,6 +23,6 @@ if (process.env.NODE_ENV === 'production') {
 }
 // Function to start server and handle port conflicts
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5003;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
